dbbc64f41eae6b9b8b06068d06c0fdbb9b8d6d62
sundarrr <sundarrathinavel1@gmail.com>
2025-07-05 23:45:51 -0400

    initial commit

---
+++ src/test/java/com/e_commerce/Store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_commerce.Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
